15f6f19340ad8cd3ba60c60be18202d11ecdbf90
Silvio Lima <silviolima07@gmail.com>
2025-02-15 23:56:46 -0300

    Changed version crewai

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ import base64
 
 #import pysqlite3 as sqlite3
 #__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 
